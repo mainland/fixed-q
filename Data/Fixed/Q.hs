8dd87e0634c05edaa4cd16398bb37354dc227e16
Geoffrey Mainland <mainland@apeiron.net>
2020-07-18 16:45:57 -0400

    Initial import.

---
+++ Data/Fixed/Q.hs
@@ -0,0 +1,379 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- |
+-- Module      :  Data.Fixed.Q
+-- Copyright   :  (c) 2020 Drexel University
+-- License     :  BSD-style
+-- Maintainer  :  mainland@drexel.edu
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- This module defines signed and unsigned Q format fixed-point numbers.
+
+module Data.Fixed.Q (
+    FixedBits(..),
+
+    UQ(..),
+    Q(..)
+  ) where
+
+import Data.Bits
+import Data.Proxy
+import Data.Ratio (denominator, numerator, (%))
+import GHC.TypeLits
+#if defined(MPFR)
+import Numeric.Rounded
+#endif /* defined(MPFR) */
+
+-- | The 'FixedBits' class denotes fixed-point types with finite bits.
+class FiniteBits a => FixedBits a where
+    -- | Number of bits use to represent sign of fixed-point number.
+    signBitSize :: a -> Int
+
+    -- | Number of bits use to represent integral portion of fixed-point number.
+    intBitSize :: a -> Int
+
+    -- | Number of bits use to represent fractional portion of fixed-point number.
+    fracBitSize :: a -> Int
+
+-- | Unsigned Q format fixed-point number with @m@ integer bits and @f@
+-- fractional bits.
+newtype UQ (m :: Nat) (f :: Nat) = UQ Integer
+  deriving (Eq, Ord)
+
+mkUQ :: forall m f . (KnownNat m, KnownNat f) => Integer -> UQ m f
+mkUQ x = UQ (x `mod` (2 ^ finiteBitSize (undefined :: UQ m f)))
+
+#if MPFR
+instance (KnownNat m, KnownNat f, KnownNat (m + f)) => Show (UQ m f) where
+#else /* !defined(MPFR) */
+instance (KnownNat m, KnownNat f) => Show (UQ m f) where
+#endif /* !defined(MPFR) */
+
+    show (UQ x) | fracbits == 0 = show x
+#if MPFR
+                | otherwise     = show (fromIntegral x / 2 ^ fracbits :: Rounded 'TowardNearest (m+f))
+#else /* !defined(MPFR) */
+                | otherwise     = show (fromIntegral x / 2 ^ fracbits :: Double)
+#endif /* !defined(MPFR) */
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: UQ m f)
+
+instance (KnownNat m, KnownNat f) => Bits (UQ m f) where
+    UQ x .&.   UQ y = UQ (x .&. y)
+    UQ x .|.   UQ y = UQ (x .|. y)
+    UQ x `xor` UQ y = UQ (x `xor` y)
+
+    complement (UQ x) = mkUQ (complement x)
+
+    zeroBits = UQ 0
+
+    isSigned _ = False
+
+    testBit (UQ x) = testBit x
+
+    bit i = mkUQ (shiftL 1 i)
+
+    shift (UQ x) i = mkUQ (shift x i)
+
+    rotate x i =
+        mkUQ $ foldr (.|.) 0 [if testBit x j then 1 `shift` ((i+j) `mod` n) else 0 |
+                              j <- [0..n-1]]
+      where
+        n = finiteBitSize x
+
+    popCount (UQ x) = popCount x
+
+    bitSizeMaybe x = Just $ finiteBitSize x
+
+    bitSize = finiteBitSize
+
+instance (KnownNat m, KnownNat f) => FiniteBits (UQ m f) where
+    finiteBitSize x = intBitSize x + fracBitSize x
+
+instance (KnownNat m, KnownNat f) => FixedBits (UQ m f) where
+    signBitSize _ = 0
+
+    intBitSize _ = fromIntegral $ natVal (Proxy :: Proxy m)
+
+    fracBitSize _ = fromIntegral $ natVal (Proxy :: Proxy f)
+
+instance (KnownNat m, KnownNat f) => Enum (UQ m f) where
+    fromEnum (UQ x) = fromEnum x
+    toEnum n = mkUQ (toInteger n)
+
+instance (KnownNat m, KnownNat f) => Bounded (UQ m f) where
+    minBound = UQ 0
+    maxBound = UQ (2 ^ finiteBitSize (undefined :: UQ m f) - 1)
+
+instance (KnownNat m, KnownNat f) => Num (UQ m f) where
+    UQ x + UQ y = mkUQ (x + y)
+    UQ x - UQ y = mkUQ (x - y)
+    UQ x * UQ y = mkUQ ((x * y) `shiftR` fracbits)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: UQ m f)
+
+    abs x         = x
+    signum (UQ 0) = 0
+    signum _      = 1
+    fromInteger i = mkUQ (i `shift` fracbits)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: UQ m f)
+
+instance (KnownNat m, KnownNat f) => Real (UQ m f) where
+    toRational (UQ x) = x % (2 ^ fracbits)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: UQ m f)
+
+instance KnownNat m => Integral (UQ m 0) where
+    toInteger (UQ x) = x
+
+    quotRem (UQ x) (UQ y) = (mkUQ q, mkUQ r)
+      where
+        (q, r) = quotRem x y
+
+instance (KnownNat m, KnownNat f, f ~ (1+f')) => Fractional (UQ m f) where
+    fromRational r = mkUQ (numerator r * 2^fracbits `div` denominator r)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: UQ m f)
+
+    UQ x / UQ y = mkUQ ((x * 2^fracbits) `div` y)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: UQ m f)
+
+instance (KnownNat m, KnownNat f, f ~ (1+f')) => RealFrac (UQ m f) where
+    properFraction x = (i, x - fromIntegral i)
+      where
+        i = truncate x
+
+    truncate x = truncate (toRational x)
+    round x    = round (toRational x)
+    ceiling x  = ceiling (toRational x)
+    floor x    = floor (toRational x)
+
+-- | Signed Q format fixed-point number with @m@ integer bits and @f@ fractional
+-- bits.
+newtype Q (m :: Nat) (f :: Nat) = Q Integer
+  deriving (Eq, Ord)
+
+mkQ :: forall m f . (KnownNat m, KnownNat f) => Integer -> Q m f
+mkQ x | even q    = Q r
+      | otherwise = Q (r - m)
+  where
+    (q, r) = divMod x m
+    m = 2 ^ finiteBitSize (undefined :: UQ m f)
+
+#if MPFR
+instance (KnownNat m, KnownNat f, KnownNat (m + f)) => Show (Q m f) where
+#else /* !defined(MPFR) */
+instance (KnownNat m, KnownNat f) => Show (Q m f) where
+#endif /* !defined(MPFR) */
+
+    show (Q x) | fracbits == 0 = show x
+#if MPFR
+               | otherwise     = show (fromIntegral x / 2 ^ fracbits :: Rounded 'TowardNearest (m+f))
+#else /* !defined(MPFR) */
+               | otherwise     = show (fromIntegral x / 2 ^ fracbits :: Double)
+#endif /* !defined(MPFR) */
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: Q m f)
+
+instance (KnownNat m, KnownNat f) => Bits (Q m f) where
+    Q x .&.   Q y = Q (x .&. y)
+    Q x .|.   Q y = Q (x .|. y)
+    Q x `xor` Q y = Q (x `xor` y)
+
+    complement (Q x) = mkQ (complement x)
+
+    zeroBits = Q 0
+
+    isSigned _ = True
+
+    testBit (Q x) = testBit x
+
+    bit i = mkQ (shiftL 1 i)
+
+    shift (Q x) i = mkQ (shift x i)
+
+    rotate x i =
+        mkQ $ foldr (.|.) 0 [if testBit x j then 1 `shift` ((i+j) `mod` n) else 0 |
+                             j <- [0..n-1]]
+      where
+        n = finiteBitSize x
+
+    popCount q@(Q x)
+      | x >= 0    = popCount x
+      | otherwise = finiteBitSize q - popCount (-x - 1)
+
+    bitSizeMaybe x = Just $ finiteBitSize x
+
+    bitSize = finiteBitSize
+
+instance (KnownNat m, KnownNat f) => FiniteBits (Q m f) where
+    finiteBitSize x = 1 + intBitSize x + fracBitSize x
+
+instance (KnownNat m, KnownNat f) => FixedBits (Q m f) where
+    signBitSize _ = 1
+
+    intBitSize _ = fromIntegral $ natVal (Proxy :: Proxy m)
+
+    fracBitSize _ = fromIntegral $ natVal (Proxy :: Proxy f)
+
+instance (KnownNat m, KnownNat f) => Enum (Q m f) where
+    fromEnum (Q x) = fromEnum x
+    toEnum n = mkQ (toInteger n)
+
+instance (KnownNat m, KnownNat f) => Bounded (Q m f) where
+    minBound = Q (-m)
+      where
+        m = 2 ^ (finiteBitSize (undefined :: Q m f) - 1)
+
+    maxBound = Q (m - 1)
+      where
+        m = 2 ^ (finiteBitSize (undefined :: Q m f) - 1)
+
+instance (KnownNat m, KnownNat f) => Num (Q m f) where
+    Q x + Q y = mkQ (x + y)
+    Q x - Q y = mkQ (x - y)
+    Q x * Q y = mkQ ((x * y) `shiftR` fracbits)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: Q m f)
+
+    abs (Q x)     = mkQ (abs x)
+    signum (Q x)  = Q (signum x)
+    fromInteger i = mkQ (i `shift` fracbits)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: Q m f)
+
+instance (KnownNat m, KnownNat f) => Real (Q m f) where
+    toRational (Q x) = x % (2 ^ fracbits)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: Q m f)
+
+instance KnownNat m => Integral (Q m 0) where
+    toInteger (Q x) = x
+
+    quotRem (Q x) (Q y) = (mkQ q, mkQ r)
+      where
+        (q, r) = quotRem x y
+
+#if MPFR
+instance (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f')) => Fractional (Q m f) where
+    fromRational r = mkQ (round (fromRational r * 2^fracbits :: Rounded 'TowardNearest (m+f)))
+#else /* !defined(MPFR) */
+instance (KnownNat m, KnownNat f, f ~ (1+f')) => Fractional (Q m f) where
+    fromRational r = mkQ (round (fromRational r * 2^fracbits :: Double))
+#endif /* !defined(MPFR) */
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: Q m f)
+
+    Q x / Q y = mkQ (x * 2^fracbits `div` y)
+      where
+        fracbits :: Int
+        fracbits = fracBitSize (undefined :: Q m f)
+
+instance (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f')) => RealFrac (Q m f) where
+    properFraction x = (i, x - fromIntegral i) where
+        i = truncate x
+
+    truncate x = truncate (toRational x)
+    round x    = round (toRational x)
+    ceiling x  = ceiling (toRational x)
+    floor x    = floor (toRational x)
+
+#if defined(MPFR)
+type R = 'TowardNearest
+
+liftUQ0 :: (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f'))
+        => Rounded R (m+f)
+        -> UQ m f
+liftUQ0 x = fromRational (toRational x)
+
+liftUQ1 :: (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f'))
+        => (Rounded R (m+f) -> Rounded R (m+f))
+        -> UQ m f
+        -> UQ m f
+liftUQ1 f x = fromRational (toRational (f (fromRational (toRational x))))
+
+liftQ0 :: (KnownNat m, KnownNat f, KnownNat (m+f), KnownNat (1+m+f), f ~ (1+f'))
+       => Rounded R (1+m+f)
+       -> Q m f
+liftQ0 x = fromRational (toRational x)
+
+liftQ1 :: (KnownNat m, KnownNat f, KnownNat (m+f), KnownNat (1+m+f), f ~ (1+f'))
+       => (Rounded R (1+m+f) -> Rounded R (1+m+f))
+       -> Q m f
+       -> Q m f
+liftQ1 f x = fromRational (toRational (f (fromRational (toRational x))))
+#else /* !defined(MPFR) */
+liftUQ0 :: (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f'))
+        => Double
+        -> UQ m f
+liftUQ0 x = fromRational (toRational x)
+
+liftUQ1 :: (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f'))
+        => (Double -> Double)
+        -> UQ m f
+        -> UQ m f
+liftUQ1 f x = fromRational (toRational (f (fromRational (toRational x))))
+
+liftQ0 :: (KnownNat m, KnownNat f, KnownNat (1+m+f), f ~ (1+f'))
+       => Double
+       -> Q m f
+liftQ0 x = fromRational (toRational x)
+
+liftQ1 :: (KnownNat m, KnownNat f, KnownNat (1+m+f), f ~ (1+f'))
+       => (Double -> Double)
+       -> Q m f
+       -> Q m f
+liftQ1 f x = fromRational (toRational (f (fromRational (toRational x))))
+#endif /* !defined(MPFR) */
+
+instance (KnownNat m, KnownNat f, KnownNat (m+f), f ~ (1+f')) => Floating (UQ m f) where
+    pi = liftUQ0 pi
+    exp = liftUQ1 exp
+    log = liftUQ1 log
+    sin = liftUQ1 sin
+    cos = liftUQ1 cos
+    asin = liftUQ1 asin
+    acos = liftUQ1 acos
+    atan = liftUQ1 atan
+    sinh = liftUQ1 sinh
+    cosh = liftUQ1 cosh
+    asinh = liftUQ1 asinh
+    acosh = liftUQ1 acosh
+    atanh = liftUQ1 atanh
+
+instance (KnownNat m, KnownNat f, KnownNat (m+f), KnownNat (1+m+f), f ~ (1+f')) => Floating (Q m f) where
+    pi = liftQ0 pi
+    exp = liftQ1 exp
+    log = liftQ1 log
+    sin = liftQ1 sin
+    cos = liftQ1 cos
+    asin = liftQ1 asin
+    acos = liftQ1 acos
+    atan = liftQ1 atan
+    sinh = liftQ1 sinh
+    cosh = liftQ1 cosh
+    asinh = liftQ1 asinh
+    acosh = liftQ1 acosh
+    atanh = liftQ1 atanh
---
+++ tests/Main.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) 2020 Drexel University
+-- License     :  BSD-style
+-- Maintainer  :  mainland@drexel.edu
+
+module Main (
+    main
+  ) where
+
+import Data.Bits
+import Data.Fixed.Q
+import Data.Int
+import Data.Word
+import Test.HUnit (Assertion, (@?=))
+import Test.Hspec
+import Test.QuickCheck (Property,
+                        (===),
+                        property)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    describe "Q (signed)" uqspec
+    describe "Unsigned Q" qspec
+
+uqspec :: Spec
+uqspec = do
+    it "bitwise and" $ property prop_uq_and
+    it "bitwise or" $ property prop_uq_or
+    it "bitwise xor" $ property prop_uq_xor
+    it "bitwise complement" $ property prop_uq_complement
+    it "bit shift" $ property prop_uq_shift
+    it "bit rotate" $ property prop_uq_rotate
+    it "bit popCount" $ property prop_uq_popCount
+
+qspec :: Spec
+qspec = do
+    it "bitwise and" $ property prop_q_and
+    it "bitwise or" $ property prop_q_or
+    it "bitwise xor" $ property prop_q_xor
+    it "bitwise complement" $ property prop_q_complement
+    it "bit shift" $ property prop_q_shift
+    it "bit rotate" $ property prop_q_rotate
+    it "bit popCount" $ property prop_q_popCount
+
+testBits1 :: (Bits a, Integral a, Bits b, Show b)
+          => (Integer -> b)
+          -> (forall c . Bits c => c -> c)
+          -> a
+          -> Property
+testBits1 k op x = op x' === k (fromIntegral (op x))
+  where
+    x' = k (fromIntegral x)
+
+testBits2 :: (Bits a, Integral a, Bits b, Show b)
+          => (Integer -> b)
+          -> (forall c . Bits c => c -> c -> c)
+          -> a
+          -> a
+          -> Property
+testBits2 k op x y = x' `op` y' === k (fromIntegral (x `op` y))
+  where
+    x' = k (fromIntegral x)
+    y' = k (fromIntegral y)
+
+testBitsResult :: (Bits a, Integral a, Bits b, Eq d, Show d)
+               => (Integer -> b)
+               -> (forall c . Bits c => c -> d)
+               -> a
+               -> Property
+testBitsResult k op x = op x' === op x
+  where
+    x' = k (fromIntegral x)
+
+testBitsInt :: (Bits a, Integral a, Bits b, Show b)
+            => (Integer -> b)
+            -> (forall c . Bits c => c -> Int -> c)
+            -> a
+            -> Int
+            -> Property
+testBitsInt k op x i = x' `op` i === k (fromIntegral (x `op` i))
+  where
+    x' = k (fromIntegral x)
+
+prop_uq_and :: Word32 -> Word32 -> Property
+prop_uq_and = testBits2 (UQ :: Integer -> UQ 32 0) (.&.)
+
+prop_uq_or :: Word32 -> Word32 -> Property
+prop_uq_or = testBits2 (UQ :: Integer -> UQ 32 0) (.|.)
+
+prop_uq_xor :: Word32 -> Word32 -> Property
+prop_uq_xor = testBits2 (UQ :: Integer -> UQ 32 0) xor
+
+prop_uq_complement :: Word32 -> Property
+prop_uq_complement = testBits1 (UQ :: Integer -> UQ 32 0) complement
+
+prop_uq_shift :: Word32 -> Int -> Property
+prop_uq_shift = testBitsInt (UQ :: Integer -> UQ 32 0) shift
+
+prop_uq_rotate :: Word32 -> Int -> Property
+prop_uq_rotate = testBitsInt (UQ :: Integer -> UQ 32 0) rotate
+
+prop_uq_popCount :: Word32 -> Property
+prop_uq_popCount = testBitsResult (UQ :: Integer -> UQ 32 0) popCount
+
+prop_q_and :: Int32 -> Int32 -> Property
+prop_q_and = testBits2 (Q :: Integer -> Q 31 0) (.&.)
+
+prop_q_or :: Int32 -> Int32 -> Property
+prop_q_or = testBits2 (Q :: Integer -> Q 31 0) (.|.)
+
+prop_q_xor :: Int32 -> Int32 -> Property
+prop_q_xor = testBits2 (Q :: Integer -> Q 31 0) xor
+
+prop_q_complement :: Int32 -> Property
+prop_q_complement = testBits1 (Q :: Integer -> Q 31 0) complement
+
+prop_q_shift :: Int32 -> Int -> Property
+prop_q_shift = testBitsInt (Q :: Integer -> Q 31 0) shift
+
+prop_q_rotate :: Int32 -> Int -> Property
+prop_q_rotate = testBitsInt (Q :: Integer -> Q 31 0) rotate
+
+prop_q_popCount :: Int32 -> Property
+prop_q_popCount = testBitsResult (Q :: Integer -> Q 31 0) popCount
+
+{-
+return []
+
+runTests :: IO Bool
+runTests = $forAllProperties $
+  quickCheckWithResult (stdArgs {maxSuccess = 1000})
+-}
